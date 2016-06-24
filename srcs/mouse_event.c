613099770597d33586e5d4bf5ba853344b166e02
Remi FRISCCA <rfriscca@e3r8p6.42.fr>
2016-06-24 13:48:45 +0200

    can't segfault when dezooming carpet

--- srcs/mouse_event.c
+++ srcs/mouse_event.c
@@ -6,7 +6,7 @@
 /*   By: rfriscca <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/22 15:05:05 by rfriscca          #+#    #+#             */
-/*   Updated: 2016/06/24 13:39:56 by rfriscca         ###   ########.fr       */
+/*   Updated: 2016/06/24 13:46:44 by rfriscca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ int		mouse_event_carpet(int button, int x, int y, t_stock *param)
 	mlx_destroy_image(param->mlx, param->img);
 	if (button == 1)
 		add = add + 100;
-	if (button == 2)
+	if (button == 2 && add > 0)
 		add = add - 100;
 	param->img = mlx_new_image(param->mlx, param->width + add,
 			param->height + add);
